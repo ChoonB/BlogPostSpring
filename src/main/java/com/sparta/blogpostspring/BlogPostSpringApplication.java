2e239499742bad7bca5501048deae0018bf2bc2e
ChoonB <shgkdc@gmail.com>
2023-02-28 13:16:33 +0900

    첫 파일 커밋

---
+++ src/main/java/com/sparta/blogpostspring/BlogPostSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.blogpostspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPostSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogPostSpringApplication.class, args);
+    }
+
+}
