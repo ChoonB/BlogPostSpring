0acd90b8a3fdbe53f88771d43ba6e12a80d00e8f
ChoonB <shgkdc@gmail.com>
2023-03-06 13:47:46 +0900

    230306 Spring 과제 Lv.2 User Entity

---
+++ src/main/java/com/sparta/blogpostspring/entity/User.java
@@ -0,0 +1,28 @@
+package com.sparta.blogpostspring.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "users")
+@NoArgsConstructor
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+
+}
