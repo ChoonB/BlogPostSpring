08348b862f9be77204d027ca811a2d67f9b2c311
ChoonB <shgkdc@gmail.com>
2023-03-06 22:06:11 +0900

    4차 수정

--- src/main/java/com/sparta/blogpostspring/dto/LoginRequestDto.java
+++ src/main/java/com/sparta/blogpostspring/dto/LoginRequestDto.java
@@ -3,9 +3,15 @@ package com.sparta.blogpostspring.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class LoginRequestDto {
+
+    @NotBlank
     private String username;
+
+    @NotBlank
     private String password;
 }
--- src/main/java/com/sparta/blogpostspring/dto/PostRequestDto.java
+++ src/main/java/com/sparta/blogpostspring/dto/PostRequestDto.java
@@ -3,9 +3,15 @@ package com.sparta.blogpostspring.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Setter
 @Getter
 public class PostRequestDto {
+
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String content;
 }
