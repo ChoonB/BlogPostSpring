7b354ec0be789cdcd0cec528603fc9b7f123782e
ChoonB <shgkdc@gmail.com>
2023-03-07 11:51:34 +0900

    패스워드 특수문자 추가

--- src/main/java/com/sparta/blogpostspring/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/blogpostspring/dto/SignupRequestDto.java
@@ -9,10 +9,12 @@ import javax.validation.constraints.Pattern;
 @Setter
 public class SignupRequestDto {
 
-    @Pattern(regexp = "^[a-z0-9]{4,10}", message = "유저 이름은 최소 4자 이상, 10자 이하이며 알파벳 소문자와 숫자만 사용해야 합니다.")
+    @Pattern(regexp = "^[a-z0-9]{4,10}",
+            message = "유저 이름은 최소 4자 이상, 10자 이하이며 알파벳 소문자와 숫자만 사용해야 합니다.")
     private String username;
 
-    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}", message = "비밀번호는 최소 8자 이상, 15자 이하이며 알파벳 대소문자와 숫자만 사용해야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9.*~`!@#$%^&()+_;:,/?|=]{8,15}",
+            message = "비밀번호는 최소 8자 이상, 15자 이하이며 알파벳 대소문자와 숫자, 특수문자만 사용해야 합니다.")
     private String password;
 
     private boolean admin = false;
