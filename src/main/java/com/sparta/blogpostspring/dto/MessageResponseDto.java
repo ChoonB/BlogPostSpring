2d80af2297afa9c5c88e0569140c7cb624393e93
ChoonB <shgkdc@gmail.com>
2023-03-07 11:31:16 +0900

    dto명칭 변경

--- src/main/java/com/sparta/blogpostspring/dto/MessageResponseDto.java
+++ src/main/java/com/sparta/blogpostspring/dto/MessageResponseDto.java
@@ -2,12 +2,14 @@ package com.sparta.blogpostspring.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @Setter
 public class MsgResponseDto {
     private String msg;
     private int statusCode;
+
     public MsgResponseDto(String msg, int statusCode) {
         this.msg = msg;
         this.statusCode = statusCode;
