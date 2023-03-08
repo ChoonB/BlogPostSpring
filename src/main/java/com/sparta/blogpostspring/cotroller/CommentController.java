fdfc4e30b68132bc3323e3f3fd8a7dcbf069e4d1
ChoonB <shgkdc@gmail.com>
2023-03-08 10:34:00 +0900

    CommentController 수정

--- src/main/java/com/sparta/blogpostspring/cotroller/CommentController.java
+++ src/main/java/com/sparta/blogpostspring/cotroller/CommentController.java
@@ -17,23 +17,24 @@ public class CommentController {
 
     private final CommentService commentService;
 
-//    api/post{postId}/commnet/{commentId}
-//    1. 댓글 작성 API 여기서 id는 Post의 id
+//    1. 댓글 작성 API
     @PostMapping("/{postId}/comment")
     public CommentResponseDto createComment(
             @PathVariable Long postId, @RequestBody CommentRequestDto commentRequestDto, HttpServletRequest request) {
         return commentService.createComment(postId, commentRequestDto, request);
     }
 
-//    2. 댓글 수정 API 여기서 id는 post의 id
+//    아래는 commentId를 commentRequestDto에 넣으려 했으나 그렇게 될 경우 delete가 body로 보낼 content가 없어서 @Pathvariable로 처리
+
+//    2. 댓글 수정 API ResponseEntity<?>를 사용해서 MessageResponseDto나 CommentResponseDto중 서비스에서 경우에따라 다르게 반환
     @PutMapping("/{postId}/comment/{commentId}")
-    public CommentResponseDto updateComment(
+    public ResponseEntity<?> updateComment(
             @PathVariable Long postId, @PathVariable Long commentId
             ,@RequestBody CommentRequestDto commentRequestDto, HttpServletRequest request) {
         return commentService.updateComment(postId, commentId ,commentRequestDto, request);
     }
 
-//    3. 댓글 삭제 API 여기서 id는 post의 id
+//    3. 댓글 삭제 API
     @DeleteMapping("/{postId}/comment/{commentId}")
     public ResponseEntity<MessageResponseDto> deleteComment(
             @PathVariable Long postId, @PathVariable Long commentId ,HttpServletRequest request) {
