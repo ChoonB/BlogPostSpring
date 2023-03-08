e5afc70aad575c9a1ad07b2ab3f10d5cb1c38fb2
ChoonB <shgkdc@gmail.com>
2023-03-08 14:17:33 +0900

    Swagger 적용

---
+++ src/main/java/com/sparta/blogpostspring/config/SwaggerConfiguration.java
@@ -0,0 +1,34 @@
+package com.sparta.blogpostspring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sparta.blogpostspring"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Practice Swagger")
+                .description("practice swagger config")
+                .version("1.0")
+                .build();
+    }
+}
