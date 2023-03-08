e47c104b0cef3b426e4fdfdaaa9c6e65043d1cc3
ChoonB <shgkdc@gmail.com>
2023-03-08 12:18:49 +0900

    Handler에 @Valid 예외 추가

--- src/main/java/com/sparta/blogpostspring/exeption/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/blogpostspring/exeption/GlobalExceptionHandler.java
@@ -3,15 +3,30 @@ package com.sparta.blogpostspring.exeption;
 import com.sparta.blogpostspring.dto.MessageResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+//    throw로 발생시킨 오류 처리
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<MessageResponseDto> handleRuntimeException(RuntimeException e){
         MessageResponseDto messageResponseDto = new MessageResponseDto(e.getMessage(), HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(messageResponseDto, messageResponseDto.getStatus());
     }
+
+//    @Valid 오류 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<MessageResponseDto> handleNotValidException(MethodArgumentNotValidException e){
+        String message = e.getFieldError().getDefaultMessage();
+        MessageResponseDto messageResponseDto = new MessageResponseDto(message, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(messageResponseDto, messageResponseDto.getStatus());
+    }
+
+
+
 }
