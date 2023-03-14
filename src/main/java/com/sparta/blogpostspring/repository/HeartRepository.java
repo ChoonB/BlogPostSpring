88d8c8a79b4075328cf9512ee243c01e33cc96dd
ChoonB <shgkdc@gmail.com>
2023-03-14 16:41:48 +0900

    좋아요 Repository 추가

---
+++ src/main/java/com/sparta/blogpostspring/repository/HeartRepository.java
@@ -0,0 +1,13 @@
+package com.sparta.blogpostspring.repository;
+
+import com.sparta.blogpostspring.entity.Comment;
+import com.sparta.blogpostspring.entity.Heart;
+import com.sparta.blogpostspring.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface HeartRepository extends JpaRepository<Heart, Long> {
+    List<Heart> findAllByPost(Post post);
+    List<Heart> findAllByComment(Comment comment);
+}
