efebb92595ce491fa459eee507855ccb719fe34c
ChoonB <shgkdc@gmail.com>
2023-03-15 11:02:04 +0900

    LV.5 추가기능 회원탈퇴

--- src/main/java/com/sparta/blogpostspring/cotroller/UserController.java
+++ src/main/java/com/sparta/blogpostspring/cotroller/UserController.java
@@ -3,9 +3,12 @@ package com.sparta.blogpostspring.cotroller;
 import com.sparta.blogpostspring.dto.LoginRequestDto;
 import com.sparta.blogpostspring.dto.SignupRequestDto;
 import com.sparta.blogpostspring.dto.MessageResponseDto;
+import com.sparta.blogpostspring.security.UserDetailsImpl;
 import com.sparta.blogpostspring.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -27,6 +30,13 @@ public class UserController {
     @PostMapping("/login")
     public MessageResponseDto login(@RequestBody @Valid LoginRequestDto loginRequestDto, HttpServletResponse response) {
         return userService.login(loginRequestDto, response);
+    }
+
+//    회원탈퇴는 이미 로그인 한 상태에서 유저의 ID와 비밀번호를 다시한번 확인.
+    @Secured("ROLE_USER")
+    @DeleteMapping("/withdrawal")
+    public MessageResponseDto withdraw(@RequestBody @Valid LoginRequestDto loginRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return userService.withdraw(loginRequestDto, userDetails.getUser());
     }
 
 
