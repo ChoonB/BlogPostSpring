359f32fe4cfecb21a9c8bf42ef431acc0a05ca8d
ChoonB <shgkdc@gmail.com>
2023-03-15 12:05:50 +0900

    좋아요에 대댓글 ID 추가

--- src/main/java/com/sparta/blogpostspring/entity/Heart.java
+++ src/main/java/com/sparta/blogpostspring/entity/Heart.java
@@ -27,11 +27,16 @@ public class Heart {
     @JoinColumn(name = "COMMENT_ID", nullable = true)
     private Comment comment;
 
+    @ManyToOne
+    @JoinColumn(name = "SUBCOMMENT_ID", nullable = true)
+    private SubComment subComment;
+
 //    게시글 좋아요
     public Heart(Post post, User user) {
         this.post = post;
         this.user = user;
         this.comment = null;
+        this.subComment = null;
     }
 
 //    댓글 좋아요
@@ -39,5 +44,14 @@ public class Heart {
         this.user = user;
         this.comment = comment;
         this.post = null;
+        this.subComment = null;
+    }
+
+//    대댓글 좋아요
+    public Heart(SubComment subComment, User user) {
+        this.user = user;
+        this.subComment = subComment;
+        this.post = null;
+        this.comment = null;
     }
 }
