19fc96fa26eae715890a8c16ea3cc19f547944cb
ChoonB <shgkdc@gmail.com>
2023-03-15 15:59:23 +0900

    대댓글 좋아요 구현

--- src/main/java/com/sparta/blogpostspring/repository/HeartRepository.java
+++ src/main/java/com/sparta/blogpostspring/repository/HeartRepository.java
@@ -3,6 +3,7 @@ package com.sparta.blogpostspring.repository;
 import com.sparta.blogpostspring.entity.Comment;
 import com.sparta.blogpostspring.entity.Heart;
 import com.sparta.blogpostspring.entity.Post;
+import com.sparta.blogpostspring.entity.SubComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,6 +11,8 @@ import java.util.List;
 public interface HeartRepository extends JpaRepository<Heart, Long> {
     List<Heart> findAllByPost(Post post);
     List<Heart> findAllByComment(Comment comment);
+    List<Heart> findAllBySubComment(SubComment subComment);
     void deleteAllByComment(Comment comment);
     void deleteAllByPost(Post post);
+    void deleteAllBySubComment(SubComment subComment);
 }
