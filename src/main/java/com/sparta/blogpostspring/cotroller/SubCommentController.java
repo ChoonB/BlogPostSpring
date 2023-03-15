81928369bead4f25013fbd5d867e9176096f9c73
ChoonB <shgkdc@gmail.com>
2023-03-15 12:05:39 +0900

    대댓글 컨트롤러

--- src/main/java/com/sparta/blogpostspring/cotroller/SubCommentController.java
+++ src/main/java/com/sparta/blogpostspring/cotroller/SubCommentController.java
@@ -1,6 +1,7 @@
 package com.sparta.blogpostspring.cotroller;
 
 import com.sparta.blogpostspring.dto.CommentRequestDto;
+import com.sparta.blogpostspring.dto.MessageResponseDto;
 import com.sparta.blogpostspring.dto.SubCommentResponseDto;
 import com.sparta.blogpostspring.security.UserDetailsImpl;
 import com.sparta.blogpostspring.service.SubCommentService;
@@ -24,7 +25,19 @@ public class SubCommentController {
         return subCommentService.createSubComment(commentId, commentRequestDto, userDetails.getUser());
     }
 
+//    2. 대댓글 수정
+    @PutMapping("/{commentId}/subcomment/{subCommentId}")
+    public SubCommentResponseDto updateSubComment(@PathVariable Long commentId, @PathVariable Long subCommentId,
+                                                  @RequestBody @Valid CommentRequestDto commentRequestDto,
+                                                  @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return subCommentService.updateSubComment(commentId, subCommentId, commentRequestDto, userDetails.getUser());
+    }
 
-
+//    3. 대댓글 삭제
+    @DeleteMapping("/{commentId}/subcomment/{subCommentId}")
+    public MessageResponseDto deleteSubComment(@PathVariable Long commentId, @PathVariable Long subCommentId,
+                                               @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return subCommentService.deleteSubComment(commentId, subCommentId, userDetails.getUser());
+    }
 
 }
