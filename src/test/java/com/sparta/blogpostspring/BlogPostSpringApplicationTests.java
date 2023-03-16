b637a06a93457194ed85fb3f53777189900b5304
ChoonB <shgkdc@gmail.com>
2023-03-16 12:02:47 +0900

    테스트 업로드2

--- src/test/java/com/sparta/blogpostspring/BlogPostSpringApplicationTests.java
+++ src/test/java/com/sparta/blogpostspring/BlogPostSpringApplicationTests.java
@@ -12,11 +12,6 @@ class BlogPostSpringApplicationTests {
 
     @Test
     void contextLoads() {
-        List<Comment> comment = new ArrayList<>();
-        if (comment != null) System.out.println("111");
-        if (!comment.isEmpty()) System.out.println("222");
-        if (!(comment.size()==0)) System.out.println("333");
-
     }
 
 }
