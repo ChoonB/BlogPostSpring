e508e323667d2adb611354842700a51aca5c30d3
ChoonB <shgkdc@gmail.com>
2023-03-22 11:33:04 +0900

    jasypt 로 DB정보 암호화 & application.properties를 .yml로 변경

---
+++ src/test/java/com/sparta/blogpostspring/config/JasyptConfigTest.java
@@ -0,0 +1,28 @@
+package com.sparta.blogpostspring.config;
+
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JasyptConfigTest {
+
+//    @Test
+//    void jasypt_test(){
+//        String t1 = "jdbc:h2:mem:db;MODE=MYSQL";
+//
+//        StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
+//        jasypt.setPassword("Spring@!Sparta");
+//
+//        String encryptedText = jasypt.encrypt(t1);
+//        String decryptedText = jasypt.decrypt(encryptedText);
+//
+//        assertEquals(decryptedText, t1);
+//
+//        System.out.println(encryptedText);
+//        System.out.println(decryptedText);
+//
+//    }
+
+}
